938b63999adb8575038703f34ee11dbb2e6ba52e
jocnn <ijann_@outlook.com>
2022-07-01 20:02:57 -0300

    se definio ruta de nuevo usuario

---
+++ routes/usersRouter.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const userController = require('../controllers/usersController')
+
+router.post('/',
+  userController.newUser
+)
+
+module.exports = router

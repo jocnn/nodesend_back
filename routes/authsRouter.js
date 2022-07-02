6d4a5d501ca6f5a8c62f9e2411bd611fdac7d228
jocnn <ijann_@outlook.com>
2022-07-02 12:08:55 -0300

    se validan los campos del formulario

--- routes/authsRouter.js
+++ routes/authsRouter.js
@@ -3,7 +3,13 @@ const router = express.Router();
 const authController = require("../controllers/authsController");
 const { check } = require("express-validator");
 
-router.post("/", authController.authenticateUser);
+router.post(
+  "/",
+  [
+    check("email").isEmail().withMessage("Agrega un email válido"),
+    check("password").not().isEmpty().withMessage("El password no puede ir vacío"),
+  ],
+  authController.authenticateUser);
 
 router.get("/", authController.authenticatedUser);
 

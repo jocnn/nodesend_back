4174979878e4273c58610fe01289429ae049a16d
jocnn <ijann_@outlook.com>
2022-07-02 08:35:07 -0300

    hasheando password

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,15 +1,24 @@
 const Users = require('../models/usersModel')
+const bcrypt = require('bcrypt')
 
 exports.newUser = async (req, res) => {
-  const { email } = req.body
+  const { email, password } = req.body
 
   let user = await Users.findOne({ email })
 
   if (user) {
     return res.status(400).json({ msg: "El usuario ya esta registrado" })
-  } 
+  }
 
-  res.json({msg: "Usuario creado correctamente"})
-  user = await new Users(req.body)
-  user.save()
+  user = new Users(req.body)
+
+  const salt = await bcrypt.genSalt(10)
+  user.password = await bcrypt.hash(password, salt)
+
+  try {
+    await user.save()
+    res.json({msg: "Usuario creado correctamente"})
+  } catch (error) {
+    console.log(error)
+  }
 }

df3d8e4847a4c332284449ebf02f5a716e91bca6
jocnn <ijann_@outlook.com>
2022-07-02 11:06:50 -0300

    se busca si el usuario existe

--- controllers/authsController.js
+++ controllers/authsController.js
@@ -1,9 +1,19 @@
-
+const User = require('../models/usersModel')
 
 exports.authenticateUser = async (req, res, next) => {
+
+  const { email } = req.body
+
+  const user = await User.findOne({ email })
+  console.log(user)
+
+  if (!user) {
+    res.status(401).json({ msg: "El usuario no existe" })
+    return next()
+  }
 
 }
 
 exports.authenticatedUser = async (req, res, next) => {
-  
+
 }

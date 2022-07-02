fddf2731526f7a59594bc5b3e5822a977c107c45
jocnn <ijann_@outlook.com>
2022-07-02 11:26:29 -0300

    se verifica la password

--- controllers/authsController.js
+++ controllers/authsController.js
@@ -1,14 +1,27 @@
 const User = require('../models/usersModel')
+const bcrypt = require('bcrypt')
 
 exports.authenticateUser = async (req, res, next) => {
 
-  const { email } = req.body
+  const { email, password } = req.body
 
   const user = await User.findOne({ email })
-  console.log(user)
 
   if (!user) {
     res.status(401).json({ msg: "El usuario no existe" })
+    return next()
+  }
+
+  console.log("🚀 ~ file: authsController.js ~ line 15 ~ exports.authenticateUser= ~ user", user)
+  console.log('usuario existe')
+
+  if (bcrypt.compareSync(password, user.password)) {
+    console.log(
+      "🚀 ~ file: authsController.js ~ line 20 ~ exports.authenticateUser= ~ compareSync",
+      "El Password es correcto"
+	  )
+  } else {
+    res.status(401).json({ msg: "Password incorrecto" })
     return next()
   }
 

1b793b6fa5babef72cc7f5a54b6ccafa2ac2f97c
jocnn <ijann_@outlook.com>
2022-07-02 09:21:09 -0300

    generando respuesta a las validaciones del router

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,14 @@
 const Users = require('../models/usersModel')
 const bcrypt = require('bcrypt')
+const { validationResult } = require('express-validator')
 
 exports.newUser = async (req, res) => {
+
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()})
+  }
+
   const { email, password } = req.body
 
   let user = await Users.findOne({ email })

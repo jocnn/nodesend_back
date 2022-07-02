afb491f9a06326aac93e6bd23ee14847268a2d54
jocnn <ijann_@outlook.com>
2022-07-02 08:21:24 -0300

    se evitó el duplicado de email en el registro

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,15 @@
 const Users = require('../models/usersModel')
 
 exports.newUser = async (req, res) => {
-  const user = await new Users(req.body)
-  user.save()
+  const { email } = req.body
+
+  let user = await Users.findOne({ email })
+
+  if (user) {
+    return res.status(400).json({ msg: "El usuario ya esta registrado" })
+  } 
+
   res.json({msg: "Usuario creado correctamente"})
+  user = await new Users(req.body)
+  user.save()
 }

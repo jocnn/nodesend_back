0b39716bc4f9c262bbdae9ecc095b5a7fd47aecd
jocnn <ijann_@outlook.com>
2022-07-02 12:11:44 -0300

    se exponen los errores del formulario

--- controllers/authsController.js
+++ controllers/authsController.js
@@ -1,12 +1,17 @@
 const User = require('../models/usersModel')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-require('dotenv').config({path: '.env.development'})
+require('dotenv').config({ path: '.env.development' })
+const { validationResult } = require("express-validator");
 
 exports.authenticateUser = async (req, res, next) => {
 
-  const { email, password } = req.body
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+  }
 
+  const { email, password } = req.body
   const user = await User.findOne({ email })
 
   if (!user) {
@@ -28,7 +33,6 @@ exports.authenticateUser = async (req, res, next) => {
     res.status(401).json({ msg: "Password incorrecto" })
     return next()
   }
-
 }
 
 exports.authenticatedUser = async (req, res, next) => {

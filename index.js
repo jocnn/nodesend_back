4941ac5bb7f3c1dd9bb7db44da3354ddbb7c287c
jocnn <ijann_@outlook.com>
2022-07-02 10:21:55 -0300

    endpoint de autenticación de usuario

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 4000
 app.use(express.json())
 
 app.use('/api/users', require('./routes/usersRouter'))
+app.use('/api/auth', require('./routes/authsRouter'))
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`El servidor esta funcionando en el puerto ${port}`)

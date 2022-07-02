957902b999fb7ba2216e039a01cdf10911794b56
jocnn <ijann_@outlook.com>
2022-07-02 09:18:49 -0300

    validando campos de registro de usuario con check

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/usersController')
+const { check } = require('express-validator')
 
-router.post('/',
-  userController.newUser
-)
+router.post(
+	"/",
+	[
+		check("name").not().isEmpty().withMessage("El nombre es obligatorio"),
+		check("email")
+      .isEmail()
+      .withMessage("Agrege un email que sea válido")
+			.not()
+			.isEmpty()
+			.withMessage("El email es obligatorio"),
+		check("password")
+			.isLength({ min: 6 })
+			.withMessage("El password debe ser de al menos 6 caracteres"),
+	],
+	userController.newUser
+);
 
 module.exports = router

4bea324153ff9389b3971c29592c9e71ced54f0f
jocnn <ijann_@outlook.com>
2022-07-04 16:39:38 -0300

    se configuro la subida de archivos

--- controllers/filesController.js
+++ controllers/filesController.js
@@ -1,7 +1,32 @@
-
+const multer = require('multer')
+const shortid = require('shortid')
 
 exports.uploadFile = async (req, res, next) => {
+	const configMulter = {
+		limits: { fileSize: 1000000 },
+		storage: (fileStorage = multer.diskStorage({
+			destination: (req, file, cb) => {
+				cb(null, __dirname + '/../uploads')
+			},
+			filename: (req, file, cb) => {
+				const extension = file.mimetype.split('/')[1]
+				cb(null, `${shortid.generate()}.${extension}`)
+			},
+		})),
+	}
 
+	const upload = multer(configMulter).single('file')
+
+	upload(req, res, async (error) => {
+		console.log(req.file)
+
+		if (!error) {
+			res.json({ file: req.file.filename })
+		} else {
+			console.log(error)
+			return next()
+		}
+	})
 }
 
 exports.deleteFile = async (req, res, next) => {}

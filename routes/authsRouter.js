b4006eaaac5b38c8be1abdce3da5637a53c30dad
jocnn <ijann_@outlook.com>
2022-07-04 07:56:14 -0300

    se importo y uso en peticione get

--- routes/authsRouter.js
+++ routes/authsRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authsController");
 const { check } = require("express-validator");
+const auth = require("../middleware/authMiddleware");
 
 router.post(
   "/",
@@ -11,6 +12,10 @@ router.post(
   ],
   authController.authenticateUser);
 
-router.get("/", authController.authenticatedUser);
+router.get(
+  "/",
+  auth,
+  authController.authenticatedUser,
+);
 
 module.exports = router;

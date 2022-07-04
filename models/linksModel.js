2fbad42149fc70ca421a79689872c0b026f72613
jocnn <ijann_@outlook.com>
2022-07-04 09:50:25 -0300

    se definió el modelo

---
+++ models/linksModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const linksSchema = new Schema({
+	url: {
+		type: String,
+		required: true,
+	},
+	name: {
+		type: String,
+		required: true,
+	},
+	name_origin: {
+		type: String,
+		required: true,
+	},
+	downloads: {
+		type: Number,
+	},
+	author: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Users',
+		default: null,
+	},
+	password: {
+		type: String,
+		default: null,
+	},
+	created: {
+		type: Date,
+		default: Date.now(),
+	},
+})
+
+module.exports = mongoose.model('Links', linksSchema)

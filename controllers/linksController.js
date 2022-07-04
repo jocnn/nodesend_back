2c5f4a560ff77f7ce00985c512335158c7e7fc8e
jocnn <ijann_@outlook.com>
2022-07-04 12:01:20 -0300

    mostro errores, hasheo el password y genero id url

--- controllers/linksController.js
+++ controllers/linksController.js
@@ -1,23 +1,43 @@
 const Links = require('../models/linksModel')
 const shortid = require('shortid')
+const bcrypt = require('bcrypt')
+const { validationResult } = require('express-validator')
 
 exports.newLink = async (req, res, next) => {
-	const { name_origin, password } = req.body
 
-  const link = new Links()
-  link.password = password
-	link.url = shortid.generate()
-  link.name = shortid.generate()
-  link.name_origin = name_origin
-  link.downloads = 1
-
-	// console.log(link)
-
-  try {
-    await link.save()
-    res.json({ msg: `${link.url}` })
-    return next()
-  } catch (error) {
-    console.log(error)
+  const errors = validationResult(req)
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() })
   }
+  
+	const { name_origin } = req.body
+
+	const link = new Links()
+	link.url = shortid.generate()
+	link.name = shortid.generate()
+	link.name_origin = name_origin
+	link.downloads = 1
+
+	if (req.user) {
+		const { password, downloads } = req.body
+
+		if (downloads) {
+			link.downloads = downloads
+		}
+
+		if (password) {
+			const salt = await bcrypt.genSalt(10)
+			link.password = await bcrypt.hash(password, salt)
+		}
+
+		link.author = req.user.id
+	}
+
+	try {
+		await link.save()
+		res.json({ msg: `${link.url}` })
+		return next()
+	} catch (error) {
+		console.log(error)
+	}
 }

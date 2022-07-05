194fd16ab9c81d338f2011add70dc686e9cfeae9
jocnn <ijann_@outlook.com>
2022-07-05 08:18:41 -0300

    ruta para obtener link y eliminar archivo

--- routes/linksRouter.js
+++ routes/linksRouter.js
@@ -1,17 +1,20 @@
-const express = require("express");
-const router = express.Router();
-const linksController = require("../controllers/linksController");
-const { check } = require("express-validator");
-const auth = require("../middleware/authMiddleware");
+const express = require('express')
+const router = express.Router()
+const linksController = require('../controllers/linksController')
+const filesController = require('../controllers/filesController')
+const { check } = require('express-validator')
+const auth = require('../middleware/authMiddleware')
 
 router.post(
 	'/',
-  [
-    check('name').not().isEmpty().withMessage('El nombre es obligatorio'),
-    check('name_origin').not().isEmpty().withMessage('Sube un archivo'),
-  ],
+	[
+		check('name').not().isEmpty().withMessage('El nombre es obligatorio'),
+		check('name_origin').not().isEmpty().withMessage('Sube un archivo'),
+	],
 	auth,
 	linksController.newLink
 )
+
+router.get('/:url', linksController.getLink, filesController.deleteFile)
 
 module.exports = router

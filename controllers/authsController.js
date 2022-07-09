c0e51b8214b7e5bd2a0797ed2dda4e08987f7a2a
jocnn <ijann_@outlook.com>
2022-07-09 15:23:30 -0300

    se agregó el email a los datos del jwt token

--- controllers/authsController.js
+++ controllers/authsController.js
@@ -23,7 +23,8 @@ exports.authenticateUser = async (req, res, next) => {
     const token = jwt.sign(
       {
         id: user._id,
-        name: user.name
+        name: user.name,
+        email: user.email,
       },
       process.env.SECRET_KEY,
       { expiresIn: '8h' });
